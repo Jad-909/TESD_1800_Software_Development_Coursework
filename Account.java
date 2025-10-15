86274688f2e3b750d244871e1bd724e218e1b7f7
Jad-909 <jaredcda@gmail.com>
2025-10-15 10:38:43 -0600

    Account Assignment
    This is an assignment from my class to create a rectangle.

---
+++ Account.java
@@ -0,0 +1,70 @@
+/*
+* Author: Jared Dan
+* Date: 10/15/2025
+*/
+import java.util.Date;
+
+public class Account {
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0; 
+    private Date dateCreated;
+
+    public Account() {
+        dateCreated = new Date();
+    }
+
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+        dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return (annualInterestRate / 100) / 12;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Error: Insufficient funds.");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
